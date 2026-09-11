1a49851eaeddcf8ba578185c0115641ae2f95e56
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-11 05:06:31 +0000

    blog: The Bit That Remembers Who Called (sstatus-SPP, 2026-09-11 01:06 EDT)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260911-045452';
+const VERSION = 'v20260911-050616';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
