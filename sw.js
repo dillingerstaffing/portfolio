bc61ce8b2bb2d612a034a61f37fa25ff71dd0845
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-11 12:27:43 +0000

    blog: The Brake Pedal on the Stopwatch (mcountinhibit)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260911-100609';
+const VERSION = 'v20260911-120802';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
