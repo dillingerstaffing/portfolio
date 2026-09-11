4571cfe65b174a8a1956bdc2d5c3da29411d8b79
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-10 20:15:17 -0400

    EQUIVALENT C: no hardware-test overclaim

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260911-001059';
+const VERSION = 'v20260911-001516';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
