ee02c7562fa752546e727781eb6570d3395811e2
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-12 07:10:06 +0000

    blog: The Write That Sleeps (user/pipebuf showpiece)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260912-060736';
+const VERSION = 'v20260912-070853';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
