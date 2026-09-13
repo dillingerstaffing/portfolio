e03ba168408b8020adcc8e123f44935be2d0b11e
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-13 22:09:34 +0000

    footer: link sibling sites in Elsewhere line

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260913-152354';
+const VERSION = 'v20260913-220921';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
