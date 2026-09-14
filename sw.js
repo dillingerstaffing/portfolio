84477b834cc63a9f0d0af91ba45e7d2f6285a283
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-14 06:10:52 +0000

    refresh service worker cache stamp

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260913-220921';
+const VERSION = 'v20260914-061043';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
