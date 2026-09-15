742fc08a8dedc26fa8472c6e6edab13f0db125a9
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-15 06:39:50 -0400

    Add Ghostlight to elsewhere links

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260914-152208';
+const VERSION = 'v20260914-160317';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
