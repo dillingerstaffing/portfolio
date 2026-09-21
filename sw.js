e18faf7dd0085a680df8a16971f4381f58e09f6e
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-21 02:14:46 -0400

    mark swap section as dated reader-prompted update on the-most-honest-lie-in-c

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260921-061132';
+const VERSION = 'v20260921-061435';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
