04922a29d03e2df1b5601c0e8c72027253dda1d4
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-21 11:20:43 +0000

    signal wire: add RP2350 laser glitch, Ubuntu post-RVA23 guide, SiFive AMD ROCm demo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260921-061435';
+const VERSION = 'v20260921-112032';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
