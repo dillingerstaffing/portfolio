fb380d2ceba3a3a0a9016aff60b2beb9eeca510d
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-21 10:28:47 -0400

    plain-language why for register saving in It's a Trap

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260921-142422';
+const VERSION = 'v20260921-142837';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
