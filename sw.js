2ce64fb27acbb4a22c6d386b9c25ae1efe6ab000
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-22 15:53:19 +0000

    add blog post: The Extension You Cannot Disassemble (Ztso fence deletion)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260922-123042';
+const VERSION = 'v20260922-155217';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
