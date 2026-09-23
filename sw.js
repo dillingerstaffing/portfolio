ae60eb29b22cf19f04952475bb3203a609a7977b
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-23 12:57:05 +0000

    add mideleg-seip-clear proof card

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260922-155217';
+const VERSION = 'v20260923-125643';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
