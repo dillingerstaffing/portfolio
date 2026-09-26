7b2a9c29646f5ba1f3b8fb5e23d0416eb9c7e1b8
dillingerstaffing <dillingerstaffing@users.noreply.github.com>
2026-09-26 20:47:30 +0000

    fix broken xv6 source link (mit-pdps to mit-pdos)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated: updates on every deploy to force SW refresh
-const VERSION = 'v20260926-153334';
+const VERSION = 'v20260926-204649';
 const CACHE = 'portfolio-' + VERSION;
 const OFFLINE_URL = 'index.html';
 
